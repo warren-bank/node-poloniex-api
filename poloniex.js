858935313b7a876e285ceb996d3f63edb8aee935
Warren R. Bank <warren.r.bank@gmail.com>
2017-07-02 18:26:17 -0700

    improve ability to catch and report "JSON-PARSE" errors

--- poloniex.js
+++ poloniex.js
@@ -141,13 +141,18 @@ function Poloniex(api_key, api_secret, opt){
             data += chunk
           });
           res.on('end', () => {
-            data = JSON.parse(data)
+            try {
+              data = JSON.parse(data)
+            }
+            catch(error) {
+              error.code = 'JSON-PARSE'
+              error.api_response = data
+              return reject(error)
+            }
             if (data && data.error){
-              reject(new Error(data.error))
+              return reject(new Error(data.error))
             }
-            else {
-              resolve(data)
-            }
+            resolve(data)
           });
         })
 
